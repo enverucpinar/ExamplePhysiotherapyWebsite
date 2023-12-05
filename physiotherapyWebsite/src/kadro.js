d7ce1e4bfbc0af2451ae14eae68d8bf8ce4949d7
enverucpinar <ucpinarenver@gmail.com>
2023-12-06 01:41:09 +0300

    kadro.js eklendi

---
+++ physiotherapyWebsite/src/kadro.js
@@ -0,0 +1,31 @@
+export const kadro = [
+  {
+    id: 1,
+    isim: "Doktor Levent",
+    mail: "doktorlevent@gmail.com",
+    telefon: "05353556554",
+    fotograf: "https://d.haberciniz.biz/other/2022/03/19/4-001.jpg",
+    ozgecmis:
+      "lorem dbsabfdhsaf fdhsabfdhsaf dfhsafvbdhsavf fdjsabfjdasbfd fdsajfbdshjaf fdsvajfvdjsavf fbdjsafjdsavf  fjdvsafhds fvdhsafvhdsa fvhdsvafhds",
+  },
+  {
+    id: 2,
+    isim: "Doktor Ela",
+    mail: "doktorela@gmail.com",
+    telefon: "05353556554",
+    fotograf:
+      "https://i.pinimg.com/736x/19/f9/af/19f9af1b86d3a0c2625331df0c5c0f21.jpg",
+    ozgecmis:
+      "lorem dbsabfdhsaf fdhsabfdhsaf dfhsafvbdhsavf fdjsabfjdasbfd fdsajfbdshjaf fdsvajfvdjsavf fbdjsafjdsavf  fjdvsafhds fvdhsafvhdsa fvhdsvafhds",
+  },
+  {
+    id: 3,
+    isim: "Enes Furkan ÅžAÅžMAZ",
+    mail: "enesfurkansasmaz@gmail.com",
+    telefon: "05353556554",
+    fotograf:
+      "https://media.licdn.com/dms/image/D4D03AQESUKfllXLVzQ/profile-displayphoto-shrink_800_800/0/1669803023396?e=1703721600&v=beta&t=iCAgCJ1ztOYIoOVFq2P6rHUyaGMRM3zoIyBxNz5XUVs",
+    ozgecmis:
+      "lorem dbsabfdhsaf fdhsabfdhsaf dfhsafvbdhsavf fdjsabfjdasbfd fdsajfbdshjaf fdsvajfvdjsavf fbdjsafjdsavf  fjdvsafhds fvdhsafvhdsa fvhdsvafhds",
+  },
+];
